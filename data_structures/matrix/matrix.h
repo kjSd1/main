03ece22aa48fc8ed938c73f00baa6da8490d86bc
kjSd1 <cska_yfr@mail.ru>
2022-03-04 15:23:01 +0300

    memory usage of matrix

--- data_structures/matrix/matrix.h
+++ data_structures/matrix/matrix.h
@@ -1,6 +1,8 @@
 #ifndef VSE_MATRIX_H
 #define VSE_MATRIX_H
 
+#include "malloc.h"
+
 typedef struct matrix {
     int **values; // элементы матрицы
     int nRows;    // количество рядов
@@ -11,5 +13,17 @@ typedef struct position{
     int rowIndex;
     int colIndex;
 }position;
+
+// Возвращает матрицу размером nRows на nCols
+matrix getMemMatrix(int nRows, int nCols);
+
+// Размещает в динамической памяти массив из nMatrices матриц размером nRows на nCols
+matrix* getMemArrayOfMatrices(int nMatrices, int nRows, int nCols);
+
+// Освобождает память, выделенную под хранение матрицы m. Процесс освобождения изображен на рисунке
+void freeMemMatrix(matrix *m);
+
+// Освобождает память, выделенную под хранение массива ms из nMatrices матриц
+void freeMemMatrices(matrix *ms, int nMatrices);
 
 #endif //VSE_MATRIX_H
